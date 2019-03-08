fdd8f8e6bd37d2cf7864b728df6682209b79129a
sergey <sergeymykytchyn@gmail.com>
2019-03-08 16:39:06 +0200

    Connected to Database through ado.net EDM (without check mark)

---
+++ FashionHousesProject/FashionHousesProject/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.Entity;
+
+namespace FashionHousesProject
+{
+    public partial class Form1 : Form
+    {
+        private ClassLibraryFashionHousesEF.FashionHousesEntities ctx;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ctx = new ClassLibraryFashionHousesEF.FashionHousesEntities();
+
+            ctx.Clothes.Load();
+            ctx.ClothesInShop.Load();
+            ctx.Designers.Load();
+            ctx.Divisions.Load();
+            ctx.FashionHouses.Load();
+            ctx.Presidents.Load();
+            ctx.ShopClothes.Load();
+        }
+    }
+}
